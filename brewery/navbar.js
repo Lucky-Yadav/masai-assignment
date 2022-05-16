5841a3f37a5e05d8c330fabdbd79d2a08a53dd97
Luckyyadav420 <97690042+Luckyyadav420@users.noreply.github.com>
2022-05-16 21:55:04 +0530

    brewery assignment

---
+++ brewery/navbar.js
@@ -0,0 +1,16 @@
+function navbar() {
+    return `<div class="fixed_header">
+    <div id="navbar">
+        <div class="logo">
+        <a href="./main.html"><p>Home</p></a>            
+        </div>
+        <div class="options">
+            <a href="brewery.html"><p>search</p></a> 
+            
+         </div>
+        </div>
+    </div>`;
+    
+}
+
+export default navbar;
